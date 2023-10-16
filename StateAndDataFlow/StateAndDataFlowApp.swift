15dad015d75dd7e53cf8d551deb265ab262d9ca6
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-10-16 11:52:24 +0300

    Initial Commit

---
+++ StateAndDataFlow/StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowApp.swift
+//  StateAndDataFlow
+//
+//  Created by Goodwasp on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
